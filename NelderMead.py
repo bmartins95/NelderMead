6056f0a28668b0a7a0bf3005880ea0e2a061312c
Bruno Martins <bruno.martins.cesfi@gmail.com>
2021-01-21 13:45:35 -0300

    first commit

---
+++ NelderMead.py
@@ -0,0 +1,123 @@
+import numpy as np
+
+class NelderMead:
+    def __init__(self, f_variables, alpha = 1.0, gamma = 2.0, beta = 0.5, delta = 0.5, use_shrink = False):
+        if not (isinstance(f_variables, list) or isinstance(f_variables, np.ndarray)):
+            raise TypeError("f_variables should be numpy.ndarray or list, i.e. mutable!")
+        else:
+            self.f_var = f_variables
+
+        self.alpha = alpha
+        self.gamma = gamma
+        self.beta = beta
+        self.delta = delta
+        self.use_shrink = use_shrink
+        self.f_values = np.zeros(len(f_variables) + 1)
+        self.gather_calls = 0
+        self.current_phase = "gather"
+
+        self.buildSimplexPoints()
+
+    def run(self, f_value):
+        if self.gather_calls < len(self.f_var):
+            self.gatherRoutine(f_value)
+        else:
+            self.optimizeRoutine(f_value)
+
+    def gatherRoutine(self, f_value):
+        if self.gather_calls == 0:
+            self.f_values[-1] = f_value
+        else:
+            self.f_values[self.gather_calls-1] = f_value
+
+        for index,value in enumerate(self.simplex[self.gather_calls]):
+            self.f_var[index] = value
+        self.gather_calls += 1
+
+    def optimizeRoutine(self, f_value):
+        if self.gather_calls == len(self.f_var):
+            self.f_values[self.gather_calls-1] = f_value
+            self.sort()
+            self.computeCentroid()
+            self.gather_calls += 1
+            self.current_phase = "pre_reflection"
+
+        if self.current_phase == "pre_reflection":
+            self.reflection()
+        elif self.current_phase == "reflection":
+            if f_value < self.f_values[-2] and f_value >= self.f_values[0]:
+                self.simplex[-1,:] = self.x_r
+                self.f_values[-1] = f_value
+                self.sort()
+                self.computeCentroid()
+                self.reflection()
+            elif (f_value < self.f_values[0]):
+                self.f_reflection = f_value
+                self.expansion()
+            else:
+                self.contraction(f_value)
+        elif self.current_phase == "expansion":
+            if f_value < self.f_reflection:
+                self.simplex[-1,:] = self.x_e
+                self.f_values[-1] = f_value
+            else:
+                self.simplex[-1,:] = self.x_r
+                self.f_values[-1] = self.f_reflection
+            self.sort()
+            self.computeCentroid()
+            self.reflection()
+        elif self.current_phase == "contraction":
+            if f_value < self.f_values[-1] or not self.use_shrink:
+                self.simplex[-1,:] = self.x_c
+                self.f_values[-1] = f_value
+                self.sort()
+                self.computeCentroid()
+                self.reflection()
+            else:
+                self.shrink()
+
+    def buildSimplexPoints(self):
+        self.simplex = np.vstack([np.eye(len(self.f_var), dtype = float), self.f_var])
+        for index, value in enumerate(self.f_var):
+            h = 0.00025 if abs(value) < 1.0e-22 else 0.05
+            self.simplex[index,:] = self.simplex[index,:] * h + value
+
+    def sort(self):
+        indexes = np.argsort(self.f_values)
+        self.f_values = sorted(self.f_values)
+        self.simplex = self.simplex[indexes,:]
+
+    def computeCentroid(self):
+        self.c = (self.simplex[:-1,:]).mean(axis=0)
+
+    def reflection(self):
+        self.current_phase = "reflection"
+        self.x_r = self.c + self.alpha*(self.c - self.simplex[-1,:])
+        for index, value in enumerate(self.x_r):
+            self.f_var[index] = value
+
+    def expansion(self):
+        self.current_phase = "expansion"
+        self.x_e = self.c + self.gamma*(self.x_r - self.c)
+        for index,value in enumerate(self.x_e):
+            self.f_var[index] = value
+
+    def contraction(self, f_value):
+        self.current_phase = "contraction"
+        if f_value >= self.f_values[-1]:
+            self.x_c = self.c + self.beta*(self.simplex[-1,:] - self.c)
+        else:
+            self.x_c = self.c + self.beta*(self.x_r - self.c)
+        for index, value in enumerate(self.x_c):
+            self.f_var[index] = value
+
+    def shrink(self):
+        self.current_phase = "gather"
+        self.gather_calls = 1
+        for index in range(1 , self.simplex.shape[1]+1):
+            self.simplex[index,:] = self.simplex[0,:] + self.delta*(self.simplex[index,:] - self.simplex[0,:])
+
+        self.simplex = np.flip(self.simplex, 0)
+        self.f_values = np.flip(self.f_values)
+        for index, value in enumerate(self.simplex[0]):
+            self.f_var[index] = value
---
+++ NelderMeadTest.py
@@ -0,0 +1,202 @@
+import unittest
+import numpy as np
+from NelderMead import NelderMead
+
+class TestNelderMead(NelderMead):
+    def buildSimplexPoints(self):
+        self.simplex = np.vstack([np.eye(len(self.f_var), dtype = float), self.f_var])
+        for index, value in enumerate(self.f_var):
+            h = 0.00025 if abs(value) < 1.0e-22 else 0.05
+            self.simplex[index,:] = self.simplex[index,:] * h
+
+class TestInit(unittest.TestCase):
+    def setUp(self):
+        self.nelder = TestNelderMead([1.0, 2.0, 0.0])
+        self.simplex_to_check = np.array([[5.0e-02, 0.0e+00, 0.0e+00],
+                                          [0.0e+00, 5.0e-02, 0.0e+00],
+                                          [0.0e+00, 0.0e+00, 2.5e-04],
+                                          [1.0e+00, 2.0e+00, 0.0e+00]])
+
+    def testSimplexBuilder(self):
+        self.assertTrue(np.allclose(self.nelder.simplex, self.simplex_to_check))
+
+class TestGatherRoutine(unittest.TestCase):
+    def setUp(self):
+        self.f_variables = [1.0, 2.0, 0.0]
+        self.nelder = TestNelderMead(self.f_variables)
+
+        self.f_values_to_check = np.array([3.0e+00, 5.0e-02, 5.0e-02])
+
+    def testGatherRoutine(self):
+        for value in self.f_values_to_check:
+            self.assertEqual(sum(self.f_variables), value)
+            self.nelder.run(sum(self.f_variables))
+
+        self.f_values_to_check = np.array([5.0e-02, 5.0e-02, 0.0e+00, 3.0e+00])
+        self.assertTrue(np.allclose(self.nelder.f_values, self.f_values_to_check))
+
+class TestReflectionSubroutine(unittest.TestCase):
+    def setUp(self):
+        self.f_variables = [1.0, 2.0, 0.0]
+        self.nelder = TestNelderMead(self.f_variables)
+
+        self.simplex_to_check1 = np.array([[0.0e+00, 0.0e+00, 2.5e-04],
+                                          [5.0e-02, 0.0e+00, 0.0e+00],
+                                          [0.0e+00, 5.0e-02, 0.0e+00],
+                                          [1.0e+00, 2.0e+00, 0.0e+00]])
+
+        self.simplex_to_check2 = np.array([[0.0e+00, 0.0e+00, 2.5e-04],
+                                          [-2.9/3.0, -5.9/3.0, 0.0005/3.0],
+                                          [5.0e-02, 0.0e+00, 0.0e+00],
+                                          [0.0e+00, 5.0e-02, 0.0e+00]])
+
+        self.f_values_to_check1 = np.array([2.5e-04, 5.0e-02, 5.0e-02, 3.0e+00])
+        self.f_values_to_check2 = np.array([2.5e-04, 5.0e-04, 5.0e-02, 5.0e-02])
+        self.centroid_to_check1 = np.array([0.05/3.0, 0.05/3.0, 0.00025/3.0])
+        self.centroid_to_check2 = np.array([-2.75/9.0, -5.9/9.0, 1.25e-03/9.0])
+        self.x_r_to_check = np.array([-2.9/3.0, -5.9/3.0, 0.0005/3.0])
+
+    def testPreReflection(self):
+        for i in range(0, 4):
+            self.nelder.run(sum(self.f_variables))
+
+        self.assertTrue(np.allclose(self.nelder.simplex, self.simplex_to_check1))
+        self.assertTrue(np.allclose(self.nelder.f_values, self.f_values_to_check1))
+        self.assertTrue(np.allclose(self.nelder.c, self.centroid_to_check1))
+        self.assertTrue(np.allclose(self.nelder.x_r, self.x_r_to_check))
+        self.assertTrue(np.allclose(self.nelder.x_r, self.nelder.f_var))
+
+
+    def testReflection(self):
+        for i in range(0, 4):
+            self.nelder.run(sum(self.f_variables))
+
+        self.nelder.run(5.0e-04)
+        self.assertTrue(np.allclose(self.nelder.simplex, self.simplex_to_check2))
+        self.assertTrue(np.allclose(self.nelder.f_values, self.f_values_to_check2))
+        self.assertTrue(np.allclose(self.nelder.c, self.centroid_to_check2))
+
+class TestExpansionSubroutine(unittest.TestCase):
+    def setUp(self):
+        self.f_variables = [1.0, 2.0, 0.0]
+        self.nelder = TestNelderMead(self.f_variables)
+
+        self.simplex_to_check3 = np.array([[-5.85/3.0, -11.85/3.0, 7.5e-04/3.0],
+                                          [0.0e+00, 0.0e+00, 2.5e-04],
+                                          [5.0e-02, 0.0e+00, 0.0e+00],
+                                          [0.0e+00, 5.0e-02, 0.0e+00]])
+
+        self.simplex_to_check4 = np.array([[-2.9/3.0, -5.9/3.0, 0.0005/3.0],
+                                          [0.0e+00, 0.0e+00, 2.5e-04],
+                                          [5.0e-02, 0.0e+00, 0.0e+00],
+                                          [0.0e+00, 5.0e-02, 0.0e+00]])
+
+        self.f_values_to_check3 = np.array([1.0e-04, 2.5e-04, 5.0e-02, 5.0e-02])
+        self.f_values_to_check4 = np.array([2.0e-04, 2.5e-04, 5.0e-02, 5.0e-02])
+        self.x_e_to_check = np.array([-5.85/3.0, -11.85/3.0, 7.5e-04/3.0])
+
+    def testExpansionBegin(self):
+        for i in range(0, 4):
+            self.nelder.run(sum(self.f_variables))
+
+        self.nelder.run(2.0e-04)
+        self.assertTrue(np.allclose(self.nelder.x_e, self.x_e_to_check))
+        self.assertTrue(np.allclose(self.nelder.f_var, self.x_e_to_check))
+
+    def testGoodExpansion(self):
+        for i in range(0, 4):
+            self.nelder.run(sum(self.f_variables))
+
+        self.nelder.run(2.0e-04)
+        self.nelder.run(1.0e-04)
+        self.assertTrue(np.allclose(self.nelder.simplex, self.simplex_to_check3))
+        self.assertTrue(np.allclose(self.nelder.f_values, self.f_values_to_check3))
+
+    def testBadExpansion(self):
+        for i in range(0, 4):
+            self.nelder.run(sum(self.f_variables))
+
+        self.nelder.run(2.0e-04)
+        self.nelder.run(3.0e-04)
+        self.assertTrue(np.allclose(self.nelder.simplex, self.simplex_to_check4))
+        self.assertTrue(np.allclose(self.nelder.f_values, self.f_values_to_check4))
+
+class TestContractionSubroutine(unittest.TestCase):
+    def setUp(self):
+        self.f_variables = [1.0, 2.0, 0.0]
+        self.nelder = TestNelderMead(self.f_variables)
+
+        self.simplex_to_check5 = np.array([[0.0e+00, 0.0e+00, 2.5e-04],
+                                          [5.0e-02, 0.0e+00, 0.0e+00],
+                                          [0.0e+00, 5.0e-02, 0.0e+00],
+                                          [5.08333333e-01, 1.00833333e+00, 4.16666667e-05]])
+
+        self.simplex_to_check6 = np.array([[0.0e+00, 0.0e+00, 2.5e-04],
+                                          [5.0e-02, 0.0e+00, 0.0e+00],
+                                          [0.0e+00, 5.0e-02, 0.0e+00],
+                                          [-4.75e-01, -9.75e-01,  1.25e-04]])
+
+        self.f_values_to_check5 = np.array([2.5e-04, 5.0e-02, 5.0e-02, 2.0e+00])
+        self.f_values_to_check6 = np.array([2.5e-04, 5.0e-02, 5.0e-02, 1.0e+00])
+        self.centroid_to_check3 = np.array([1.66666667e-02, 1.66666667e-02, 8.33333333e-05])
+        self.centroid_to_check4 = np.array([1.66666667e-02, 1.66666667e-02, 8.33333333e-05])
+        self.x_c_inside_to_check = np.array([5.08333333e-01, 1.00833333e+00, 4.16666667e-05])
+        self.x_c_outside_to_check = np.array([-4.75e-01, -9.75e-01,  1.25e-04])
+
+    def testContractionInside(self):
+        for i in range(0, 4):
+            self.nelder.run(sum(self.f_variables))
+
+        self.nelder.run(4.0e+00)
+        self.nelder.run(2.0e+00)
+        self.assertTrue(np.allclose(self.nelder.simplex, self.simplex_to_check5))
+        self.assertTrue(np.allclose(self.nelder.f_values, self.f_values_to_check5))
+        self.assertTrue(np.allclose(self.nelder.c, self.centroid_to_check3))
+        self.assertTrue(np.allclose(self.nelder.x_c, self.x_c_inside_to_check))
+
+    def testContractionOutside(self):
+        for i in range(0, 4):
+            self.nelder.run(sum(self.f_variables))
+
+        self.nelder.run(2.0e+00)
+        self.nelder.run(1.0e+00)
+        self.assertTrue(np.allclose(self.nelder.simplex, self.simplex_to_check6))
+        self.assertTrue(np.allclose(self.nelder.f_values, self.f_values_to_check6))
+        self.assertTrue(np.allclose(self.nelder.c, self.centroid_to_check4))
+        self.assertTrue(np.allclose(self.nelder.x_c, self.x_c_outside_to_check))
+
+class TestShrinkSubroutine(unittest.TestCase):
+    def setUp(self):
+        self.f_variables = [1.0, 2.0, 0.0]
+        self.nelder = TestNelderMead(self.f_variables)
+
+        self.simplex_to_check7 = np.array([[5.0e-01, 1.0e+00, 1.25e-04],
+                                          [0.0e+00, 2.5e-02, 1.25e-04],
+                                          [2.5e-02, 0.0e+00, 1.25e-04],
+                                          [0.0e+00, 0.0e+00, 2.5e-04]])
+
+        self.simplex_to_check8 = np.array([[0.0e+00, 0.0e+00, 2.5e-04],
+                                          [0.0e+00, 2.5e-02, 1.25e-04],
+                                          [2.5e-02, 0.0e+00, 1.25e-04],
+                                          [5.0e-01, 1.0e+00, 1.25e-04]])
+
+        self.f_values_to_check7 = np.array([2.5e-04, 2.5125e-02, 2.5125e-02, 1.500125e+00])
+
+    def testShrink(self):
+        self.nelder = TestNelderMead(self.f_variables, use_shrink=True)
+
+        for i in range(0, 4):
+            self.nelder.run(sum(self.f_variables))
+
+        self.nelder.run(4.0e+00)
+        self.nelder.run(5.0e+00)
+        self.assertTrue(np.allclose(self.nelder.simplex, self.simplex_to_check7))
+
+        for i in range(0,3):
+            self.nelder.run(sum(self.f_variables))
+
+        self.assertTrue(np.allclose(self.nelder.simplex, self.simplex_to_check8))
+        self.assertTrue(np.allclose(self.nelder.f_values, self.f_values_to_check7))
+
+if __name__ == '__main__':
+    unittest.main()
